0fa4516b2b6bd18cfaf7a36ffdf020f6a70170bd
Nastyshka14 <nastya.korshun.1997@mail.ru>
2022-06-08 20:13:22 +0400

    kek

--- server/app.js
+++ server/app.js
@@ -23,9 +23,11 @@ app.use('/api/auth', require('./routes/auth.routes'));
 async function start() {
   try {
     // await mongoose.connect(config.get('mongoUri'), {
+      console.log('STAAAAAAAAAART')
     await mongoose
       .connect(process.env.DB_URL || 'mongodb+srv://nastya:kokokosha14@cluster0.lzznh.mongodb.net/?retryWrites=true&w=majority')
       .then(() => console.log('CONNNNNNECTED TO DB'))
+      .catch((err) => console.log('NOT CONNECTEEEEEEEEEEEEEEEEED!!!!'))
     
     app.listen(PORT, () =>
       console.log(`App has been started on port ${PORT}...`)

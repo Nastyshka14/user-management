efbaaffb0b7f3e488b9f4ab02b928bde60225272
Nastyshka14 <nastya.korshun.1997@mail.ru>
2022-06-08 20:18:03 +0400

    lol

--- server/app.js
+++ server/app.js
@@ -12,14 +12,6 @@ app.use(express.json({ extended: true }));
 
 app.use('/api/auth', require('./routes/auth.routes'));
 
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use('/', express.static(path.join(__dirname, 'client', 'build')));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.request(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-
 async function start() {
   try {
     // await mongoose.connect(config.get('mongoUri'), {
@@ -27,7 +19,7 @@ async function start() {
     await mongoose
       .connect(process.env.DB_URL || 'mongodb+srv://nastya:kokokosha14@cluster0.lzznh.mongodb.net/?retryWrites=true&w=majority')
       .then(() => console.log('CONNNNNNECTED TO DB'))
-      .catch((err) => console.log('NOT CONNECTEEEEEEEEEEEEEEEEED!!!!'))
+      .catch((err) => console.log('NOT CONNECTEEEEEEEEEEEEEEEEED!!!!: ', { err }))
     
     app.listen(PORT, () =>
       console.log(`App has been started on port ${PORT}...`)

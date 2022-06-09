e09943d5d88548bee0981568dfd85c0efa2ba81d
Anastasiya Chura <60580806+Nastyshka14@users.noreply.github.com>
2022-06-09 09:55:17 +0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,8 +14,6 @@ app.use('/api/auth', require('./routes/auth.routes'));
 
 async function start() {
   try {
-    // await mongoose.connect(config.get('mongoUri'), {
-      console.log('STAAAAAAAAAART')
     await mongoose
       .connect(process.env.DB_URL || 'mongodb+srv://nastya:kokokosha14@cluster0.lzznh.mongodb.net/?retryWrites=true&w=majority')
       .then(() => console.log('CONNNNNNECTED TO DB'))
